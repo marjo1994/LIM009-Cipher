edfd1170ea05fb45199fde44058ebac607b9305e
marjo1994 <m_rhl@live.com>
2019-02-21 08:51:45 -0500

    Agregando estructura de primera y segunda pantalla

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-/* Acá va tu código */
+// Funcion Guardar clave
+
+document.getElementById("btnS").addEventListener("click",save);
+
+function save() {
+const name = document.getElementById("name").value;
+document.getElementById("saludo").innerHTML=`Hola ${name} , tu clave elegida es`;
+let key = document.getElementById("key").value;
+document.getElementById("key2").innerHTML=`${key}`;
+document.getElementById("inicio").style.display="none";
+document.getElementById("claves").style.display="block";
+}

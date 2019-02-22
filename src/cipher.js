fdf952003ddc37804c01d28bfa3c2f08a0e9a032
marjo1994 <m_rhl@live.com>
2019-02-22 13:16:34 -0500

    Función encriptar y en proceso función descifrar

--- src/cipher.js
+++ src/cipher.js
@@ -1,14 +1,18 @@
-const encodeKey = document.getElementById(encodeKey);
-const key2 = document.getElementById(key2);
+const decodeKey = document.getElementById("decode-key");
+const encodeKey = document.getElementById("encode-key");
+const key2 = document.getElementById("key2");
+const number = 10;
 
 window.cipher = {
   encode: (number,key2) => {
     for (let i = 0; i< key2.length; i++) {
-      encodeKey.innerHTML += String.fromChardCode((key2.charCodeAt(i)-65+number)%26+65);
+      encodeKey.innerHTML += String.fromCharCode(((key2.charCodeAt(i)-65+number)%26)+65);
       }
     },
-  //decode: (number,encodeKey) => {
-    //for (let i = 0; i< encodeKey.length; i++) {
-    //decodeKey.innerHTML += String.fromChardCode((encodeKey.charCodeAt(i)-65-number)%26+65);
-    }
+  decode: (number,encodeKey) => {
+    for (let i = 0; i< encodeKey.length; i++) {
+      decodeKey.innerHTML += String.fromCharCode(((encodeKey.charCodeAt(i)-65-number)%26)+65);
+   }
+  }
 };
+
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // Función para guardar clave 
 document.getElementById("btnS").addEventListener("click",save);
 
-function save() {
+function save () {
 const name = document.getElementById("name").value;
 document.getElementById("saludo").innerHTML=`Hola ${name} , tu clave elegida es`;
 let key = document.getElementById("key").value;
@@ -11,8 +11,14 @@ document.getElementById("claves").style.display="block";
 }
 //Función para devolver clave encriptada
 document.getElementById("btnEnc").addEventListener("click",enc);
-
 function enc() {
-const key2 = document.getElementById(key2);
-cipher.encode(10,key2);
+const key2 = document.getElementById("key2").textContent;
+cipher.encode(number,key2);
+}
+
+//Función para devolver clave desencriptada
+document.getElementById("btnDes").addEventListener("click",des);
+function des() {
+const encodeKey = document.getElementById("encode-key").textContent;
+cipher.decode(number,encodeKey);
 }

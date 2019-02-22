5ea1ce0207f344d3887f4c9e8ee00aac420bc084
marjo1994 <m_rhl@live.com>
2019-02-22 08:20:19 -0500

    Agregué funcionalidad de encriptado

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,14 @@
+const encodeKey = document.getElementById(encodeKey);
+const key2 = document.getElementById(key2);
+
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-  },
-  decode: () => {
-    /* Acá va tu código */
-  }
+  encode: (number,key2) => {
+    for (let i = 0; i< key2.length; i++) {
+      encodeKey.innerHTML += String.fromChardCode((key2.charCodeAt(i)-65+number)%26+65);
+      }
+    },
+  //decode: (number,encodeKey) => {
+    //for (let i = 0; i< encodeKey.length; i++) {
+    //decodeKey.innerHTML += String.fromChardCode((encodeKey.charCodeAt(i)-65-number)%26+65);
+    }
 };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-// Funcion Guardar clave
-
+// Función para guardar clave 
 document.getElementById("btnS").addEventListener("click",save);
 
 function save() {
@@ -9,4 +8,11 @@ let key = document.getElementById("key").value;
 document.getElementById("key2").innerHTML=`${key}`;
 document.getElementById("inicio").style.display="none";
 document.getElementById("claves").style.display="block";
+}
+//Función para devolver clave encriptada
+document.getElementById("btnEnc").addEventListener("click",enc);
+
+function enc() {
+const key2 = document.getElementById(key2);
+cipher.encode(10,key2);
 }

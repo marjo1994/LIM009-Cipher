aa08f113738fe60e0075a4e7c2dd551973134e15
marjo1994 <m_rhl@live.com>
2019-02-27 16:38:24 -0500

    Realicé test y agregué opcion para enc/dec números y simbolos

--- src/index.js
+++ src/index.js
@@ -1,37 +1,36 @@
 // Función para guardar clave 
-document.getElementById("btnS").addEventListener("click",save);
-function save() {
-const name = document.getElementById("name").value;
-document.getElementById("saludo").innerHTML=`Hola ${name} , tu clave elegida es`;
-let key = document.getElementById("key").value;
-document.getElementById("key2").innerHTML=`${key}`;
-document.getElementById("inicio").style.display="none";
-document.getElementById("claves").style.display="block";
-}
+document.getElementById("btnS").addEventListener("click",()=>{
+    const name = document.getElementById("name").value;
+    document.getElementById("saludo").innerHTML=`Hola ${name} , tu clave elegida es`;
+    const key = document.getElementById("key").value;
+    document.getElementById("key2").innerHTML=`${key}`;
+    document.getElementById("inicio").style.display="none";
+    document.getElementById("claves").style.display="block";
+});
+
 
 //Función para devolver clave encriptada
-document.getElementById("btnEnc").addEventListener("click",enc);
-function enc() {
-const key2 = document.getElementById("key2").textContent;
-const number = parseInt(document.getElementById("number").value);
-cipher.encode(number,key2);
+document.getElementById("btnEnc").addEventListener("click",()=>{
+const offset = parseInt(document.getElementById("number").value);
+const string = document.getElementById("key2").textContent;
+const encodeKey = document.getElementById("encode-key");
+encodeKey.innerHTML = cipher.encode(offset,string);
 document.getElementById("inicio").style.display="none";
 document.getElementById("claves").style.display="none";
 document.getElementById("clave-enc").style.display="block";
-}
+});
 
 //Función para devolver clave desencriptada
-document.getElementById("btnDes").addEventListener("click",des);
-function des() {
-const encodeKey = document.getElementById("encode-key").textContent;
-const number = parseInt(document.getElementById("number").value);
-cipher.decode(number,encodeKey);
+document.getElementById("btnDes").addEventListener("click",()=>{
+const offset = parseInt(document.getElementById("number").value);
+const decodeKey = document.getElementById("decode-key");
+const string = document.getElementById("encode-key").textContent;
+decodeKey.innerHTML = cipher.decode(offset,string);
 document.getElementById("inicio").style.display="none";
 document.getElementById("claves").style.display="none";
 document.getElementById("clave-enc").style.display="none";
 document.getElementById("clave-dec").style.display="block";
-}
-
+});
 
 //Función para refrescar la página
 

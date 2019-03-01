11cf7dbeca106fc955a5e7ebe60d88bf63dc0ef9
marjo1994 <m_rhl@live.com>
2019-03-01 11:02:38 -0500

    Testeando con lulu

--- src/cipher.js
+++ src/cipher.js
@@ -1,31 +1,5 @@
 window.cipher = {
-  encode: (offset,string) => {
-    let textenc = '';
-    for (let i = 0; i< string.length; i++) {
-      if(65<=string.charCodeAt(i) && string.charCodeAt(i)<=90) {
-        textenc += String.fromCharCode(((string.charCodeAt(i)-65+offset)%26)+65);
-      } else if(97<=string.charCodeAt(i) && string.charCodeAt(i)<=122) {
-        textenc += String.fromCharCode(((string.charCodeAt(i)-97+offset)%26)+97);
-      } else if(33<=string.charCodeAt(i) && string.charCodeAt(i)<=47) {
-        textenc += String.fromCharCode(((string.charCodeAt(i)-33+offset)%15)+33);
-      } else if(48<=string.charCodeAt(i) && string.charCodeAt(i)<=57) {
-        textenc += String.fromCharCode(((string.charCodeAt(i)-48+offset)%10)+48);
-      }
-     } return textenc;
-  },
-  decode: (offset,string) => {
-    let textdec = '';
-    for (let i = 0; i< string.length; i++) {
-      if(65<=string.charCodeAt(i) && string.charCodeAt(i)<=90) {
-        textdec += String.fromCharCode(((string.charCodeAt(i)-90-offset)%26)+90); 
-      } else if(97<=string.charCodeAt(i) && string.charCodeAt(i)<=122) {
-        textdec += String.fromCharCode(((string.charCodeAt(i)-122-offset)%26)+122);
-      } else if(33<=string.charCodeAt(i) && string.charCodeAt(i)<=47) {
-        textdec += String.fromCharCode(((string.charCodeAt(i)-47-offset)%15)+47);
-      } else if(48<=string.charCodeAt(i) && string.charCodeAt(i)<=57) {
-        textdec +=  String.fromCharCode(((string.charCodeAt(i)-57-offset)%10)+57);
-      }
-    } return textdec;
+  suma: (3,5) =>{
+    return 3+5;
   }
 };
-
---
+++ test/cipher.spec.js
@@ -0,0 +1,17 @@
+describe('cipher',()=>{
+  it('cipher debería ser un objeto',()=>{
+  assert.equal(typeof cipher,'object')
+  });
+describe('cipher.suma',()=>{
+  it('cipher.suma debería ser una función',()=>{
+  assert.equal(typeof cipher.suma,'function')
+  })
+  it('cipher suma debe sumar 2 numeros para ello cuando a>3, b=5, debe retornar 8',()=>{
+    assert.equal(cipher.suma(3,5),8)
+    })
+describe('cipher',()=>{
+  it('cipher debería ser un objeto',()=>{
+  assert.equal(typeof cipher,'object')
+   });
+  })
+});

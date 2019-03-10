b0ee67056d4b0f5cc709219d29c4bc7f09b6f512
marjo1994 <m_rhl@live.com>
2019-03-10 05:03:10 -0500

    Offset negativo para mayúsculas JS

--- src/cipher.js
+++ src/cipher.js
@@ -2,33 +2,39 @@ window.cipher = {
   encode: (offset,string) => {
     let textenc = '';
     for (let i = 0; i< string.length; i++) {
-      if(65<=string.charCodeAt(i) && string.charCodeAt(i)<=90) {
-        textenc += String.fromCharCode((string.charCodeAt(i)-65+offset)%26+65);
-      } else if(97<=string.charCodeAt(i) && string.charCodeAt(i)<=122) {
-        textenc += String.fromCharCode((string.charCodeAt(i)-97+offset)%26+97);
-      } else if(33<=string.charCodeAt(i) && string.charCodeAt(i)<=47) {
-        textenc += String.fromCharCode((string.charCodeAt(i)-33+offset)%15+33);
-      } else if(48<=string.charCodeAt(i) && string.charCodeAt(i)<=57) {
-        textenc += String.fromCharCode((string.charCodeAt(i)-48+offset)%10+48);
-      } else if(32 === string.charCodeAt(i)) {
-        textenc += String.fromCharCode(string.charCodeAt(i));
-      }
+      const Ascii = string.charCodeAt(i);
+      if(65<=Ascii && Ascii<=90 && offset>=0) {
+        textenc += String.fromCharCode((Ascii-65+offset)%26+65);
+      } else if(97<=Ascii && Ascii<=122 && offset>=0) {
+        textenc += String.fromCharCode((Ascii-97+offset)%26+97);
+      } else if(33<=Ascii && Ascii<=47 && offset>=0) {
+        textenc += String.fromCharCode((Ascii-33+offset)%15+33);
+      } else if(48<=Ascii && Ascii<=57 && offset>=0) {
+        textenc += String.fromCharCode((Ascii-48+offset)%10+48);
+      } else if(32 === Ascii && offset>=0) {
+        textenc += String.fromCharCode(Ascii);
+      } else if(65<=Ascii && Ascii<=90 && offset<0) {
+        textenc += String.fromCharCode((Ascii-90+offset)%26+90); 
+      } 
      } return textenc;
   },
   decode: (offset,string) => {
     let textdec = '';
     for (let i = 0; i< string.length; i++) {
-      if(65<=string.charCodeAt(i) && string.charCodeAt(i)<=90) {
-        textdec += String.fromCharCode((string.charCodeAt(i)-90-offset)%26+90); 
-      } else if(97<=string.charCodeAt(i) && string.charCodeAt(i)<=122) {
-        textdec += String.fromCharCode((string.charCodeAt(i)-122-offset)%26+122);
-      } else if(33<=string.charCodeAt(i) && string.charCodeAt(i)<=47) {
-        textdec += String.fromCharCode((string.charCodeAt(i)-47-offset)%15+47);
-      } else if(48<=string.charCodeAt(i) && string.charCodeAt(i)<=57) {
-        textdec +=  String.fromCharCode((string.charCodeAt(i)-57-offset)%10+57);
-      } else if(32 === string.charCodeAt(i)) {
-        textdec += String.fromCharCode(string.charCodeAt(i));
-      }
+      const Ascii = string.charCodeAt(i);
+      if(65<=Ascii && Ascii<=90 && offset>=0) {
+        textdec += String.fromCharCode((Ascii-90-offset)%26+90); 
+      } else if(97<=Ascii && Ascii<=122 && offset>=0) {
+        textdec += String.fromCharCode((Ascii-122-offset)%26+122);
+      } else if(33<=Ascii && Ascii<=47 && offset>=0) {
+        textdec += String.fromCharCode((Ascii-47-offset)%15+47);
+      } else if(48<=Ascii && Ascii<=57 && offset>=0) {
+        textdec +=  String.fromCharCode((Ascii-57-offset)%10+57);
+      } else if(32 === Ascii && offset>=0) {
+        textdec += String.fromCharCode(Ascii);
+      } else if(65<=Ascii && Ascii<=90 && offset<0) {
+        textdec += String.fromCharCode((Ascii-65-offset)%26+65);
+      } 
     } return textdec;
   }
 };
